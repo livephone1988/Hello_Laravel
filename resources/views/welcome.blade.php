a0f435c1e221903005c3801f232e8176a893012c
livephone <livephone1988@163.com>
2019-01-23 07:39:37 +0000

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My first Laravel</title>
+    <style type="text/css">
+        html,body{
+            height: 100%;
+        }
+        body{
+            margin:0;
+            padding: 0;
+            width: 100%;
+            display: table;
+            font-weight: 100%;
+            font-family:"Helvetica Neue", NotoSansHans-Regular,AvenirNext-Regular,arial,Hiragino Sans GB,"Microsoft Yahei","Hiragino Sans GB","WenQuanYi Micro Hei",sans-serif;
+            color: blue;
+        }
+        .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 166px;
+            }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="content">
+            <div class="title"> Hello World! -by BlueGhost</div>
+        </div>
+    </div>
+
+</body>
+</html>
